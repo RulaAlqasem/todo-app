f7af54c18721d7fa9c6d237b1c6c866352bdddb7
Rula Al-qasem <jaradatrula6@gmail.com>
2021-08-17 01:53:50 +0300

    ch

--- src/app.js
+++ src/app.js
@@ -1,11 +1,70 @@
-import React from 'react';
 
-import ToDo from './components/todo/todo.js';
+import React, { useEffect, useState } from 'react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <ToDo />
-    );
+
+import { v4 as uuid } from 'uuid';
+
+import ToDo from './components/todo/todo';
+import useForm from './hooks/form';
+
+import List from './components/List'
+
+
+
+function App(props) {
+
+  const [list, setList] = useState([]);
+  const [incomplete, setIncomplete] = useState([]);
+  const { handleChange, handleSubmit } = useForm(addItem);
+
+
+  function addItem(item) {
+    console.log(item);
+    item.id = uuid();
+    item.complete = false;
+    setList([...list, item]);
   }
+
+  function deleteItem(id) {
+    let items = []
+    list.map((ele, idx) => {
+      if (id === idx) {
+        return
+      } else {
+        items.push(ele)
+      }
+    })
+    setList(items);
+  }
+
+  function toggleComplete(id) {
+
+    const items = list.map((item, idx) => {
+      if (id === idx) {
+        item.complete = !item.complete;
+      }
+      return item;
+    });
+
+    setList(items);
+
+  }
+
+  useEffect(() => {
+    let incompleteCount = list.filter(item => !item.complete).length;
+    setIncomplete(incompleteCount);
+    document.title = `To Do List: ${incomplete}`;
+  }, [list]);
+
+
+
+
+  return (
+    <div>
+      <ToDo handleChange={handleChange} handleSubmit={handleSubmit} toggleComplete={toggleComplete} />
+      <List list={list} toggleComplete={toggleComplete} deleteItem={deleteItem} />
+    </div>
+  )
 }
+
+export default App
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,84 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import useForm from '../../hooks/form.js';
+import React from 'react';
+
 
 import { v4 as uuid } from 'uuid';
+import "./todo.css"
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
-const ToDo = () => {
 
-  const [list, setList] = useState([]);
-  const [incomplete, setIncomplete] = useState([]);
-  const { handleChange, handleSubmit } = useForm(addItem);
 
-  function addItem(item) {
-    console.log(item);
-    item.id = uuid();
-    item.complete = false;
-    setList([...list, item]);
-  }
+import { FormGroup, InputGroup, Button, Navbar, Label } from "@blueprintjs/core";
+// import { Label } from 'blueprint-components';
 
-  function deleteItem(id) {
-    const items = list.filter(item => item.id !== id);
-    setList(items);
-  }
 
-  function toggleComplete(id) {
+const ToDo = (props) => {
 
-    const items = list.map(item => {
-      if (item.id == id) {
-        item.complete = !item.complete;
-      }
-      return item;
-    });
-
-    setList(items);
-
-  }
-
-  useEffect(() => {
-    let incompleteCount = list.filter(item => !item.complete).length;
-    setIncomplete(incompleteCount);
-    document.title = `To Do List: ${incomplete}`;
-  }, [list]);
 
   return (
     <>
-      <header>
-        <h1>To Do List: {incomplete} items pending</h1>
-      </header>
+      <Navbar>
+        <h1>To Do List: {props.incomplete} items pending</h1>
+      </Navbar>
+      <card className='app'>
+        <FormGroup  >
 
-      <form onSubmit={handleSubmit}>
+          <h2 intent="danger">Add To Do Item</h2>
 
-        <h2>Add To Do Item</h2>
+          <Label intent="danger">
+            <span>To Do Item</span>
+            <InputGroup onChange={props.handleChange} name="text" type="text" placeholder="Item Details" intent="danger" />
+          </Label>
 
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
+          <Label intent="danger">
+            <span>Assigned To</span>
+            <InputGroup onChange={props.handleChange} name="assignee" type="text" placeholder="Assignee Name" intent="danger" />
+          </Label>
 
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
+          <Label>
+            <span>Difficulty</span>
+            < input onChange={props.handleChange} defaultValue={5} type="range" min={1} max={10} name="difficulty" intent="danger" class="range" />
+          </Label>
 
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
-        </label>
+          <Label>
+            <Button type="click" onClick={props.handleSubmit} iconName="cog" intent="danger">Add Item</Button>
+          </Label>
+        </FormGroup>
+      </card>
 
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
 
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
 
     </>
   );
